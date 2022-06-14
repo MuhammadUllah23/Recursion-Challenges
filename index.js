e563504f93cfd15b7024b261434b4513e7ba5a14
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-06-14 06:17:06 +0000

    factorial challenge solved

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function power(base, expo) {
 // Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g., factorial four (4!) is equal to 24, because 4*3*2*1 equals 24. factorial (0!) is always 1.
 
 function factorial(fact) {
-  
+  if (fact === 1 || fact === 0) return 1;
+
+  return fact * factorial(fact - 1)
 }
+
+console.log(factorial(3))
