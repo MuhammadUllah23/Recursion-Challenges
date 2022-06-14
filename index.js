96fd71eaee0909868e2a25a1a283825f811ff7f7
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-06-14 06:33:57 +0000

    recursiveRange Challenge

--- index.js
+++ index.js
@@ -36,4 +36,16 @@ function productOfArray(arr) {
   return arr[0] * productOfArray(arr.slice(1))
 }
 
-console.log(productOfArray([1, 2, 3, 10]))
+// console.log(productOfArray([1, 2, 3, 10]))
+
+// ---------------------------------------------------------------------------------
+
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
+
+function recursiveRange(num) {
+  
+}
+
+console.log(recursiveRange(6))
+
+// ---------------------------------------------------------------------------------
