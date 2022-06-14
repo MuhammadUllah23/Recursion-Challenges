ec5500b5315a045429bfeaf82a015c85baa4abaf
Muhammad Ullah <78986038+MuhammadUllah23@users.noreply.github.com>
2022-06-14 06:14:33 +0000

    factorial challenge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // console.log()
 
-// -----------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
 
 // Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents. 
 
@@ -11,3 +11,12 @@ function power(base, expo) {
   return base * power(base, expo -1)
 }
 
+// console.log(power(2, 10))
+
+// ---------------------------------------------------------------------------------
+
+// Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g., factorial four (4!) is equal to 24, because 4*3*2*1 equals 24. factorial (0!) is always 1.
+
+function factorial(fact) {
+  
+}
